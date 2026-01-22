00eae7041ce9ec3ef5ae762d030b2dd9f4942ce2
npt1225 <nguyenphuthinhse@gmail.com>
2026-01-22 15:09:55 +0700

    fix code

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -6,9 +6,9 @@ export default function Home() {
     return (
         <div>
             {/* Spinner Start */}
-            <div id="spinner" className="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
+            {/* <div id="spinner" className="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
                 <div className="spinner-grow text-primary" role="status" />
-            </div>
+            </div> */}
             {/* Spinner End */}
             {/* Navbar start */}
             <div className="container-fluid fixed-top">
