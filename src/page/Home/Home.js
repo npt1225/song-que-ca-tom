6852fffdcca0bdc99ff8ec88c534c1ec6633ef6e
npt1225 <nguyenphuthinhse@gmail.com>
2026-01-22 14:39:27 +0700

    fix img

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -88,11 +88,11 @@ export default function Home() {
                             <div id="carouselId" className="carousel slide position-relative" data-bs-ride="carousel">
                                 <div className="carousel-inner" role="listbox">
                                     <div className="carousel-item active rounded">
-                                        <img src="img/hero-img-1.png" className="img-fluid w-100 h-100 bg-secondary rounded" alt="First slide" />
+                                        {/* <img src="img/hero-img-1.png" className="img-fluid w-100 h-100 bg-secondary rounded" alt="First slide" /> */}
                                         {/* <NavLink exact to="/" className="btn px-4 py-2 text-white rounded">Fruites</NavLink> */}
                                     </div>
                                     <div className="carousel-item rounded">
-                                        <img src="img/hero-img-2.jpg" className="img-fluid w-100 h-100 rounded" alt="Second slide" />
+                                        {/* <img src="img/hero-img-2.jpg" className="img-fluid w-100 h-100 rounded" alt="Second slide" /> */}
                                         {/* <NavLink exact to="/" className="btn px-4 py-2 text-white rounded">Vesitables</NavLink> */}
                                     </div>
                                 </div>
